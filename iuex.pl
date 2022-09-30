cb2c08a0f76c112e5a7df7d035f09288f97f393c
Daniel Mantovani <dmanto@gmail.com>
2022-09-30 17:17:10 -0300

    test 3

--- iuex.pl
+++ iuex.pl
@@ -17,6 +17,7 @@ get '/spa-auth/login' => sub ($c) {
   for my $ls (@local_spas) {
     push @$select_field, [$ls->{name}, $ls->{mac}]
   };
+  push @$select_field, ['--- Other (not listed) Spa ---', -1];
   $c->stash(select_field => $select_field);
   return $c->render(template => 'index')
    if secure_compare $c->req->url->to_abs->userinfo // '', 'alexa-id:alexa-secret'; 
@@ -26,7 +27,17 @@ get '/spa-auth/login' => sub ($c) {
 
 };
 
-post '/spa-auth/user-auth' => sub ($c) {
+post '/spa-auth/spa-select' => sub ($c) {
+  $c->log->debug("params: " . $c->dumper($c->req->params));
+  if ($c->param('spa_name') eq '-1') {
+   return $c->render(template => 'paste-sharelink');
+  };
+  my $url = Mojo::URL->new($c->param('redirect_uri'));
+  $url->query(state => $c->param('state'), code => '12345678');
+  $c->redirect_to($url);
+} => 'spa-select';
+
+post '/spa-auth/paste-spa-sharelink' => sub ($c) {
   $c->log->debug("params: " . $c->dumper($c->req->params));
   if ($c->param('username') ne 'pepe' || $c->param('password') ne 'jose') {
    return $c->render(status => 404, text => 'Forbidden');
@@ -34,7 +45,8 @@ post '/spa-auth/user-auth' => sub ($c) {
   my $url = Mojo::URL->new($c->param('redirect_uri'));
   $url->query(state => $c->param('state'), code => '12345678');
   $c->redirect_to($url);
-} => 'uauth';
+} => 'paste-spa-sharelink';
+
 
 post '/spa-auth/token' => sub ($c) {
   $c->log->debug("params for token: " . $c->dumper($c->req));
@@ -64,8 +76,36 @@ __DATA__
 <body>
     <h1> Formulario de autenticacion</h1>
     <p> Tu IP es: <%= $c->stash('remote_ip') // 'unknown' %> <p>
-    %= form_for $c->url_for('uauth')->to_abs => (method => 'POST') => begin
+    %= form_for $c->url_for('spa-select')->to_abs => (method => 'POST') => begin
+        %= label_for spa_name => "Please select a spa:"
         %= select_field spa_name => stash 'select_field'
+        <input type="submit" value="Enviar">
+        %= hidden_field client_id => $c->param('client_id');
+        %= hidden_field redirect_uri => $c->param('redirect_uri');
+        %= hidden_field response_type => $c->param('response_type');
+        %= hidden_field scope => $c->param('scope');
+        %= hidden_field state => $c->param('state'); 
+        %= csrf_field
+	%= hidden_field ip => $c->tx->remote_address;
+    % end
+</body>
+</html>
+
+@@ paste_sharelink.html.ep
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UI for alexa - Paste link page</title>
+    </head>
+<body>
+    <h1> Paste share link here</h1>
+    <p> Tu IP es: <%= $c->stash('remote_ip') // 'unknown' %> <p>
+    %= form_for $c->url_for('paste-spa-sharelink')->to_abs => (method => 'POST') => begin
+        %= label_for spa_link => "Please paste a shared link here:"
+        %= text_area 'spa_link'
         <input type="submit" value="Enviar">
         %= hidden_field client_id => $c->param('client_id');
         %= hidden_field redirect_uri => $c->param('redirect_uri');
